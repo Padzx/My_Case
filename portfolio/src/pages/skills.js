e01f6b1f0f971ae6b8bf4685cad4ca00e3919e48
Padzx <padzxgabriel@gmail.com>
2024-03-11 01:21:01 -0300

    Update&Portfolio

--- portfolio/src/pages/skills.js
+++ portfolio/src/pages/skills.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './skillsComponent.css'
 import { FaReact, FaHtml5, FaCss3Alt, FaVuejs, FaJsSquare, FaJenkins, FaGitAlt, FaPython} from 'react-icons/fa';
-import { FaJava} from 'react-icons/fa';
+import { FaJava, FaNetworkWired } from 'react-icons/fa';
 import { SiDjango, SiMongodb, SiKubernetes, SiYaml, SiGithubactions, SiTerraform, SiMysql, SiApachehadoop, SiPowerbi }  from "react-icons/si";
+import { SiAmazonec2, SiAmazonrds, SiAmazondynamodb, SiAmazonsqs, SiAwslambda, SiAmazonecs, SiAmazoneks, SiAwsfargate, SiAmazons3  } from "react-icons/si";
+import { SiAmazonsimpleemailservice, SiAmazonroute53, SiAmazonredshift, SiAmazoncloudwatch, SiAmazonapigateway} from "react-icons/si";
+
 import { BiLogoFlask, BiLogoPostgresql} from "react-icons/bi";
 import { DiSpark } from "react-icons/di";
 import { TbHexagonNumber1, TbHexagonNumber2, TbHexagonNumber3 } from "react-icons/tb";
@@ -39,10 +42,13 @@ const Skills = () => {
             <DataScience />
           </>
         )}
+        {selectedContainer === 'aws' && <AWSCloud />}
+        {selectedContainer === 'others' && <OthersTools />}
       </div>
       <div className="icons-navigation">
         <TbHexagonNumber1 className="one-filter" onClick={() => handleIconClick('frontend')} />
         <TbHexagonNumber2 className="two-filter" onClick={() => handleIconClick('devops')} />
+        <TbHexagonNumber3 className="three-filter" onClick={() => handleIconClick('aws')} />
       </div>
     </div>
   );
@@ -217,32 +223,126 @@ const DataScience = () => {
     <div className="data-science-icons">
       <div className="icon-with-nivel-python">
         <FaPython className="python-icon" />
-        <span className="nivel-text-python"></span>
+        <NivelExperienciaBackend nivel={4} prefix="backend-python" />
+        <span className="nivel-text-python">Avançado</span>
       </div>
       <div className="icon-with-nivel-spark">
         <DiSpark className="spark-icon" />
-        <span className="nivel-text-spark"></span>
+        <NivelExperienciaBackend nivel={2} prefix="backend-spark" />
+        <span className="nivel-text-spark">Básico</span>
       </div>
       <div className="icon-with-nivel-mysql">
         <SiMysql className="mysql-icon" />
-        <span className="nivel-text-mysql"></span>
+        <NivelExperienciaBackend nivel={3} prefix="backend-mysql" />
+        <span className="nivel-text-mysql">Intermediário</span>
       </div>
       <div className="icon-with-nivel-apache-hadoop">
         <SiApachehadoop className="apache-hadoop-icon" />
-        <span className="nivel-text-apache-hadoop"></span>
+        <NivelExperienciaBackend nivel={2} prefix="backend-hadoop" />
+        <span className="nivel-text-apache-hadoop">Básico</span>
       </div>
       <div className="icon-with-nivel-postgresql">
         <BiLogoPostgresql className="postgresql-icon" />
-        <span className="nivel-text-postgresql"></span>
+        <NivelExperienciaBackend nivel={3} prefix="backend-postgresql" />
+        <span className="nivel-text-postgresql">Intermediário</span>
       </div>
       <div className="icon-with-nivel-powerbi">
         <SiPowerbi  className="powerbi-icon" />
-        <span className="nivel-text-mongodb"></span>
+        <NivelExperienciaBackend nivel={2} prefix="backend-powerbi" />
+        <span className="nivel-text-powerbi">Básico</span>
       </div>
 
     </div>
     </div>
 
 
+  )
+}
+
+const AWSCloud = () => {
+
+  return (
+
+    <div className='container-aws-cloud'>
+      <h1>AWS Cloud</h1>
+      <div className="aws-cloud-icons">
+      <div className="icon-with-nivel-ec2-">
+        < SiAmazonec2 className="ec2-icon" />
+        <span className="nivel-text-ec2"></span>
+      </div>
+      <div className="icon-with-nivel-s3-">
+        <  SiAmazons3 className="s3-icon" />
+        <span className="nivel-text-s3"></span>
+      </div>
+      <div className="icon-with-nivel-rds">
+        <   SiAmazonrds className="rds-icon" />
+        <span className="nivel-text-rds"></span>
+      </div>
+      <div className="icon-with-nivel-dynamodb">
+        <   SiAmazondynamodb className="dynamodb-icon" />
+        <span className="nivel-text-dynamodb"></span>
+      </div>
+      <div className="icon-with-nivel-sqs">
+        <   SiAmazonsqs className="sqs-icon" />
+        <span className="nivel-text-sqs"></span>
+      </div>
+      <div className="icon-with-nivel-lambda">
+        <   SiAwslambda  className="lambda-icon" />
+        <span className="nivel-text-lambda"></span>
+      </div>
+      <div className="icon-with-nivel-ecs">
+        <   SiAmazonecs className="ecs-icon" />
+        <span className="nivel-text-ecs"></span>
+      </div>
+      <div className="icon-with-nivel-eks">
+        <   SiAmazoneks className="eks-icon" />
+        <span className="nivel-text-eks"></span>
+      </div>
+    </div>
+    <div className="icon-with-nivel-eks">
+        <   SiAmazoneks className="eks-icon" />
+        <span className="nivel-text-eks"></span>
+    </div>
+    <div className="icon-with-nivel-fargate">
+        <   SiAwsfargate className="fargate-icon" />
+        <span className="nivel-text-fargate"></span>
+    </div>
+    <div className="icon-with-nivel-vpc">
+        <   FaNetworkWired className="vpc-icon" />
+        <span className="nivel-text-vpc"></span>
+    </div>
+    <div className="icon-with-nivel-sns">
+        <   SiAmazonsimpleemailservice className="sns-icon" />
+        <span className="nivel-text-sns"></span>
+    </div>
+    <div className="icon-with-nivel-Route53">
+        <   SiAmazonroute53 className="Route53-icon" />
+        <span className="nivel-text-Route53"></span>
+    </div>
+    <div className="icon-with-nivel-redshift">
+        <   SiAmazonredshift className="redshift-icon" />
+        <span className="nivel-text-redshift"></span>
+    </div>
+    <div className="icon-with-nivel-apigateway">
+        <   SiAmazonapigateway className="apigateway-icon" />
+        <span className="nivel-text-apigateway"></span>
+    </div>
+    <div className="icon-with-nivel-cloudwatch">
+        <   SiAmazoncloudwatch className="cloudwatch-icon" />
+        <span className="nivel-text-cloudwatch"></span>
+    </div>
+    </div>
+  )
+}
+
+const OthersTools = () => {
+
+  return (
+
+    <div className='container-others-tools'>
+      <h1>Other Tools</h1>
+      <div className="aws-others-tools-icon">
+    </div>
+    </div>
   )
 }
